1aee7f1bc6a0f73f968b1d448e5e7eae48e92409
Jin Wang <jwang@Jins-MacBook-Air.local>
2017-03-18 11:31:29 -0400

    small edits

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-var pg = require('pg')
-var retry = require('retry')
+const pg = require('pg');
+const retry = require('retry');
+const http = require('http');
+const server = http.createServer();
+const Sequelize = require('sequelize');
 
-console.log("Hello World!")
-
-var operation = retry.operation({retries:3})
+var operation = retry.operation({retries:3});
 
 operation.attempt(function() {
-  var client = new pg.Client()
-  client.connect(function(e) {
-    client.end()
+  const client = new pg.Client();
+  client.connect((e) => {
+    client.end();
     if(operation.retry(e)) {
       return;
     }
-    if(!e) console.log("Hello Postgres!")
+    if(!e) {
+      console.log("Hello Postgres!");
+    }
 
-    var http = require('http');
-    var server = http.createServer();
-    var Sequelize = require('sequelize');
-    var db = new Sequelize(process.env.DATABASE_URL);
+    const db = new Sequelize(process.env.DATABASE_URL);
 
     db.define('Sandcastles', {
       name: Sequelize.STRING
@@ -26,14 +26,15 @@ operation.attempt(function() {
     db.sync({force: true})
     .then(() => {
       server.listen(8080, () => {
-        console.log(process.env.DATABASE_URL);
-        console.log('Server is listening');
+        console.log("Database url", process.env.DATABASE_URL);
+        console.log('Server is listening on port 8080');
       });
     })
 
     server.on('request', (req, res) => {
       console.log(req.url);
-      db.model('Sandcastles').create({
+      db.model('Sandcastles')
+      .create({
         name: 'jems'
       })
       .then(() => {
@@ -49,8 +50,8 @@ operation.attempt(function() {
 
     // models.db.sync({force: true})
     //     .then(function () {
-    //         server.listen(3001, function () {
-    //             console.log('Server is listening on port 3001!');
+    //         server.listen(8080, function () {
+    //             console.log('Server is listening on port 8080!');
     //         });
     //     })
     //     .catch(console.error);
