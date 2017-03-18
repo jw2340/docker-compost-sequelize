cecaea95d92d1049109ccb0371b3dca283ce33f7
Jin Wang <jwang@Jins-MacBook-Air.local>
2017-03-18 11:35:42 -0400

    wikistack app working

--- server.js
+++ server.js
@@ -17,45 +17,44 @@ operation.attempt(function() {
       console.log("Hello Postgres!");
     }
 
-    const db = new Sequelize(process.env.DATABASE_URL);
+    // const db = new Sequelize(process.env.DATABASE_URL);
 
-    db.define('Sandcastles', {
-      name: Sequelize.STRING
-    })
+    // db.define('Sandcastles', {
+    //   name: Sequelize.STRING
+    // })
 
-    db.sync({force: true})
-    .then(() => {
-      server.listen(8080, () => {
-        console.log("Database url", process.env.DATABASE_URL);
-        console.log('Server is listening on port 8080');
-      });
-    })
+    // db.sync({force: true})
+    // .then(() => {
+    //   server.listen(8080, () => {
+    //     console.log("Database url", process.env.DATABASE_URL);
+    //     console.log('Server is listening on port 8080');
+    //   });
+    // })
 
-    server.on('request', (req, res) => {
-      console.log(req.url);
-      db.model('Sandcastles')
-      .create({
-        name: 'jems'
-      })
-      .then(() => {
-        res.write('Hello')
-        res.end();
-      })
-      .catch(console.error);
-    });
-    // var models = require('./models');
-    // var Promise = require('bluebird');
+    // server.on('request', (req, res) => {
+    //   console.log(req.url);
+    //   db.model('Sandcastles')
+    //   .create({
+    //     name: 'jems'
+    //   })
+    //   .then(() => {
+    //     res.write('Hello')
+    //     res.end();
+    //   })
+    //   .catch(console.error);
+    // });
+    var models = require('./models');
+    var Promise = require('bluebird');
 
-    // server.on('request', require('./app'));
+    server.on('request', require('./app'));
 
-    // models.db.sync({force: true})
-    //     .then(function () {
-    //         server.listen(8080, function () {
-    //             console.log('Server is listening on port 8080!');
-    //         });
-    //     })
-    //     .catch(console.error);
-
+    models.db.sync({force: true})
+        .then(function () {
+            server.listen(8080, function () {
+                console.log('Server is listening on port 8080!');
+            });
+        })
+        .catch(console.error);
   })
 })
 
